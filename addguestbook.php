93ed6351e71c65a24a505c4772d5fd94497672b4
JennyKor <59826821+JennyKor@users.noreply.github.com>
2020-02-20 08:35:11 +0200

    nimeä ja muuttujia muutettu

--- addguestbook.php
+++ addguestbook.php
@@ -7,24 +7,24 @@
 
 <?php
 
-$kayttajanimi=$_POST["nimi"];
+$kayttajanimi=$_POST["name"];
 $email=$_POST["email"];
 $kommentti=$_POST["comment"];
-$salasana=$_POST["salasana"];
+//$salasana=$_POST["salasana"];
 
 $yhteys=mysqli_connect("127.0.0.1", "trtkp19a3", "trtkp19a3");
 $ok=mysqli_select_db($yhteys, "trtkp19a3");
 
-$sql="insert into jenny19101_vieraskirja(kayttajanimi, email, kommentti, salasana) values(?, ?, ?, md5(?))";
+$sql="insert into jenny19101_vieraskirja(kayttajanimi, email, kommentti) values(?, ?, ?)";
 $stmt=mysqli_prepare($yhteys, $sql);
-mysqli_stmt_bind_param($stmt, 'ssss', $kayttajanimi, $email, $kommentti, $salasana);
+mysqli_stmt_bind_param($stmt, 'sss', $kayttajanimi, $email, $kommentti);
 mysqli_stmt_execute($stmt);
 
 mysqli_stmt_close($stmt);
 mysqli_close($yhteys);
 ?>
 <?php
-header("Location:vieraskirja.html");
+header("Location:index.html");
 exit;
 ?>
 </body>
