1deaecccfebb4f250be8f9ea34263552bf8744af
JennyKor <59826821+JennyKor@users.noreply.github.com>
2020-03-09 10:03:58 +0200

    Päivitetty current_timestamp mut ei toimi

--- vieraskirja.php
+++ vieraskirja.php
@@ -1,31 +1,35 @@
 <!DOCTYPE html>
 <html>
 <head>
+ <meta charset="UTF-8">
 <title>Kiitokset palautteesta</title>
 </head>
 <body>
 
 <?php
 
-$kayttajanimi=$_POST["nimi"];
+$name=$_POST["nimi"]; // lomakkeelta tulevat muuttujat muutettava
 $email=$_POST["email"];
-$kommentti=$_POST["comment"];
-$salasana=$_POST["salasana"];
+$message=$_POST["comment"];
+//$salasana=$_POST["salasana"];
 
 $yhteys=mysqli_connect("127.0.0.1", "trtkp19a3", "trtkp19a3");
 $ok=mysqli_select_db($yhteys, "trtkp19a3");
 
-$sql="insert into jenny19101_vieraskirja(kayttajanimi, email, kommentti, salasana) values(?, ?, ?, md5(?))";
+//mikä tässä alla olevassa heittää?
+$sql="insert into fightclub_guestbook(name, email, message, timestamp) values(?, ?, ?, CURRENT_TIMESTAMP())";
 $stmt=mysqli_prepare($yhteys, $sql);
-mysqli_stmt_bind_param($stmt, 'ssss', $kayttajanimi, $email, $kommentti, $salasana);
+mysqli_stmt_bind_param($stmt, 'ssss', $name, $email, $message, $timestamp);
 mysqli_stmt_execute($stmt);
 
 mysqli_stmt_close($stmt);
 mysqli_close($yhteys);
 ?>
+
 <?php
-header("Location:vieraskirja.html");
+header("Location:vieraskirja.html"); // muutettava!!
 exit;
 ?>
+
 </body>
 </html>
