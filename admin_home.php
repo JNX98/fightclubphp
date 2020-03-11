8e8a744b36e04432f7f01e4f4c65840f752aace5
JennyKor <59826821+JennyKor@users.noreply.github.com>
2020-03-11 07:03:34 +0200

    oikea css takaisin käyttöön

--- admin_home.php
+++ admin_home.php
@@ -7,7 +7,7 @@ if($_SESSION['level']!="admin"){
 
 <html>
 <head>
-<link rel="stylesheet" type="text/css" href="style.css">
+<link rel="stylesheet" type="text/css" href="stylesheet.css">
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 <title>Admin Home</title>
 
